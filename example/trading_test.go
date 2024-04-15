c07f3956c0ce817623ac0c40e10ed36f0d5366bd
mstmdev <mstmdev@gmail.com>
2024-04-15 16:29:02 +0800

    Update the trading example

--- example/trading_test.go
+++ example/trading_test.go
@@ -14,9 +14,9 @@ func TestTrading(t *testing.T) {
 	buy := workchain.NewWork(buy)
 	sell := workchain.NewWork(sell)
 
-	priceContext := context.WithValue(context.Background(), "code", "000001")
-	priceContext = context.WithValue(priceContext, "get_price", getPrice)
-	priceContext, cancel := context.WithCancel(priceContext)
+	ctx := context.WithValue(context.Background(), "code", "000001")
+	ctx = context.WithValue(ctx, "get_current_price", getCurrentPrice)
+	ctx, cancel := context.WithCancel(ctx)
 
 	go func() {
 		<-time.After(time.Second * 3)
@@ -27,32 +27,31 @@ func TestTrading(t *testing.T) {
 	workchain.WorkRing(
 		buy.WithCond(buyCond(30).Loop()),
 		sell.WithCond(sellCond(60).Loop())).
-		Do(priceContext)
+		Do(ctx)
 
 	fmt.Println("trading stopped!")
 }
 
 func buy(ctx context.Context) error {
-	code := ctx.Value("code").(string)
-	price := ctx.Value("price").(int)
+	code := getCode(ctx)
+	price := getPrice(ctx)
 	fmt.Printf("[%s] [buy] code=%s price=%d\n", now(), code, price)
 	return nil
 }
 
 func sell(ctx context.Context) error {
-	code := ctx.Value("code").(string)
-	price := ctx.Value("price").(int)
+	code := getCode(ctx)
+	price := getPrice(ctx)
 	fmt.Printf("[%s] [sell] code=%s price=%d\n", now(), code, price)
 	return nil
 }
 
 func buyCond(buyPrice int) workchain.CondFunc {
 	return func(ctx context.Context) (bool, context.Context, error) {
-		code := ctx.Value("code").(string)
-		priceFunc := ctx.Value("get_price").(func(code string) int)
-		price := priceFunc(code)
+		code := getCode(ctx)
+		price := getCurrentPriceFunc(ctx)(code)
 		if price < buyPrice {
-			ctx = context.WithValue(ctx, "price", price)
+			ctx = setPrice(ctx, price)
 			return true, ctx, nil
 		}
 		return false, ctx, nil
@@ -61,26 +60,41 @@ func buyCond(buyPrice int) workchain.CondFunc {
 
 func sellCond(sellPrice int) workchain.CondFunc {
 	return func(ctx context.Context) (bool, context.Context, error) {
-		code := ctx.Value("code").(string)
-		priceFunc := ctx.Value("get_price").(func(code string) int)
-		price := priceFunc(code)
+		code := getCode(ctx)
+		price := getCurrentPriceFunc(ctx)(code)
 		if price > sellPrice {
-			ctx = context.WithValue(ctx, "price", price)
+			ctx = setPrice(ctx, price)
 			return true, ctx, nil
 		}
 		return false, ctx, nil
 	}
 }
 
-func getPrice(code string) int {
+func getCurrentPrice(code string) int {
 	time.Sleep(time.Millisecond * 100)
 	n := rand.Intn(100)
 	if n == 0 {
-		return getPrice(code)
+		return getCurrentPrice(code)
 	}
 	return n
 }
 
 func now() string {
 	return time.Now().Format("2006-01-02 15:04:05.000")
+}
+
+func getCode(ctx context.Context) string {
+	return ctx.Value("code").(string)
+}
+
+func getPrice(ctx context.Context) int {
+	return ctx.Value("price").(int)
+}
+
+func setPrice(ctx context.Context, price int) context.Context {
+	return context.WithValue(ctx, "price", price)
+}
+
+func getCurrentPriceFunc(ctx context.Context) func(code string) int {
+	return ctx.Value("get_current_price").(func(code string) int)
 }
